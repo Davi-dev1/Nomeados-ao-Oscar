e9f81e3f0191b899ae9b7c2d41d324e78ecb2044
Davi_dev1 <davigancalveesdasilva@gmail.com>
2024-10-22 23:26:54 -0300

    Delete atividades_do.sql

--- atividades_do.sql
+++
@@ -1,3 +0,0 @@
-select * from indicados_ao_oscar;
-select count(*) from indicados_ao_oscar where nome_do_indicado like "%Natalie Portman%";
-select * from indicados_ao_oscar where nome_do_indicado like "%Amy Adams%" and vencedor="true"
