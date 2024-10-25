9a52e32aeecbc05afd1273ab18be6ea2e9e567f7
Davi_dev1 <davigancalveesdasilva@gmail.com>
2024-10-25 16:03:56 -0300

    Update atividades_do.sql

--- atividades_do.sql
+++ atividades_do.sql
@@ -1,5 +1,5 @@
 select * from indicados_ao_oscar;
-select count(*) from indicados_ao_oscar where nome_do_indicado like "%Natalie Portman%";
+select count(*) from indicados_ao_oscar where nome_do_indicado like "%Natalie Portman%" and vencedor="true";
 select * from indicados_ao_oscar where nome_do_indicado like "%Amy Adams%" and vencedor="true";
 select * from indicados_ao_oscar where nome_do_filme like "%Toy Story%" and vencedor="true";
 -- ou pode ser assim: --
